af415ffc8989d4409a9afdb61d0df5bd5187bb20
jads27 <jalvesdsilva27@gmail.com>
2023-07-13 17:22:26 +0200

    Création du filtrage des travaux

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,30 +1,60 @@
+const gallerySection = document.querySelector(".gallery");
+const filtersSection = document.querySelector(".filters");
+let allWorks = [];
+
+fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((data) => {
+        const allFilterButton = document.createElement("button");
+        allFilterButton.innerText = "Tous";
+        allFilterButton.classList.add("active");
+        filtersSection.appendChild(allFilterButton);
+
+        allFilterButton.addEventListener("click", () => {
+            const categoryButtons = document.querySelectorAll(".filters .active");
+            categoryButtons.forEach((button) => button.classList.remove("active"));
+            allFilterButton.classList.add("active");
+            filterWorks("all");
+        });
+
+        data.forEach((category) => {
+            const filterButtons = document.createElement("button");
+            filterButtons.innerText = category.name;
+            filtersSection.appendChild(filterButtons);
+
+            filterButtons.addEventListener("click", () => {
+                const categoryButtons = document.querySelectorAll(".filters .active");
+                categoryButtons.forEach((button) => button.classList.remove("active"));
+                filterButtons.classList.add("active");
+                filterWorks(category.id);
+            });
+        });
+    });
+
 fetch("http://localhost:5678/api/works")
-.then( data => data.json())
-.then( jsonlistFigure => {
-    for(let i = 0; i < jsonlistFigure.length; i++) {
+    .then((response) => response.json())
+    .then((data) => {
+        allWorks = data;
+        filterWorks("all");
+    });
 
+const filterWorks = (categoryId) => {
+    gallerySection.innerHTML = "";
 
-        const figure = jsonlistFigure[i];
-
-        // Récupération de l'élément du DOM qui accueillera les fiches
-        const sectionGallery = document.querySelector(".gallery")
-
-        // Création d'une balise dédiée aux travaux de l'architecte
-        const workElement = document.createElement("figure"); 
-
-        // Création des balises
-        const imageElement = document.createElement("img");
-        imageElement.src = figure.imageUrl;
-        const titleElement = document.createElement("figcaption");
-        titleElement.innerText = figure.title;
-        
-        // On rattache la balise figure à la section Gallery
-        sectionGallery.appendChild(workElement);
-        // On rattacge l'image et le titre à workElement (la balise figure)
-        workElement.appendChild(imageElement);
-        workElement.appendChild(titleElement);
-
-        console.log(titleElement)
+    if (categoryId === "all") {
+        allWorks.forEach((work) => {
+            createWorkElement(work);
+        });
+    } else {
+        const filteredWorks = allWorks.filter((work) => work.categoryId === categoryId);
+        filteredWorks.forEach((work) => {
+            createWorkElement(work);
+        });
     }
-    
-}) ;
+};
+
+const createWorkElement = (work) => {
+    const workElement = document.createElement("figure");
+    workElement.innerHTML = `<img src="${work.imageUrl}" /><figcaption>${work.title}</figcaption>`;
+    gallerySection.appendChild(workElement);
+};
