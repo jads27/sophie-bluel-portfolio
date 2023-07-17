73134952934958de28c7f8558a713b004c029361
jads27 <jalvesdsilva27@gmail.com>
2023-07-17 14:44:25 +0200

    Ajout de la gestion du focus lors de la navigation par tabulation dans la modale

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,23 +1,30 @@
 const galleryModal = document.querySelector(".modal-gallery");
 let modal = null;
+const focusableSelector = "button, input";
+let focusablesElements = [];
+let previouslyFocusedElement = null;
 
 const openModal = () => {
-    const target = document.querySelector(".modal");
-    target.style.display = "flex";
-    target.removeAttribute("aria-hidden", "false");
-    target.setAttribute("aria-modal", "true");
-    modal = target;
-    modal.addEventListener("click", closeModal);
-    modal.querySelector(".modal-close").addEventListener("click", closeModal);
-    modal.querySelector(".modal-stop").addEventListener("click", stopPropagation);
+    modal = document.querySelector(".modal");
 
     allWorks.forEach((work) => {
         createWorkElement(work, galleryModal);
     });
+
+    focusablesElements = Array.from(modal.querySelectorAll(focusableSelector));
+    modal.style.display = "flex";
+    previouslyFocusedElement = document.querySelector(":focus");
+    focusablesElements[0].focus();
+    modal.removeAttribute("aria-hidden", "false");
+    modal.setAttribute("aria-modal", "true");
+    modal.addEventListener("click", closeModal);
+    modal.querySelector(".modal-close").addEventListener("click", closeModal);
+    modal.querySelector(".modal-stop").addEventListener("click", stopPropagation);
 };
 
 const closeModal = () => {
     if (modal === null) return;
+    if (previouslyFocusedElement !== null) previouslyFocusedElement.focus();
     modal.style.display = "none";
     modal.setAttribute("aria-hidden", "true");
     modal.removeAttribute("aria-modal");
@@ -31,7 +38,32 @@ const closeModal = () => {
 const stopPropagation = (e) => {
     e.stopPropagation();
 };
+const focusInModal = (e) => {
+    e.preventDefault();
+    let index = focusablesElements.findIndex((f) => f === modal.querySelector(":focus"));
+    if (e.shiftKey === true) {
+        index--;
+    } else {
+        index++;
+    }
+    if (index >= focusablesElements.length) {
+        index = 0;
+    }
+    if (index < 0) {
+        index = focusablesElements.length - 1;
+    }
+    focusablesElements[index].focus();
+};
 
 document.querySelectorAll(".modal-open").forEach((button) => {
     button.addEventListener("click", openModal);
+});
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e);
+    }
+    if (e.key === "Tab" && modal !== null) {
+        focusInModal(e);
+    }
 });
