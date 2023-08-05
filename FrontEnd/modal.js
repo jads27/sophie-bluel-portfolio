e0f8fbe6360cf78e4f71eefc07dc518b33125b43
jads27 <jalvesdsilva27@gmail.com>
2023-08-05 19:30:37 +0200

    Création de la deuxième fenêtre modale

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -3,6 +3,8 @@ let modal = null;
 const focusableSelector = "button, input";
 let focusablesElements = [];
 let previouslyFocusedElement = null;
+const modal1 = document.querySelector("aside div:nth-child(1)");
+const modal2 = document.querySelector("aside div:nth-child(2)");
 
 const openModal = () => {
     modal = document.querySelector(".modal");
@@ -13,13 +15,30 @@ const openModal = () => {
 
     focusablesElements = Array.from(modal.querySelectorAll(focusableSelector));
     modal.style.display = "flex";
+    modal1.style.display = "flex";
+    modal2.style.display = "none";
     previouslyFocusedElement = document.querySelector(":focus");
-    focusablesElements[0].focus();
     modal.removeAttribute("aria-hidden", "false");
     modal.setAttribute("aria-modal", "true");
     modal.addEventListener("click", closeModal);
-    modal.querySelector(".modal-close").addEventListener("click", closeModal);
-    modal.querySelector(".modal-stop").addEventListener("click", stopPropagation);
+    modal.querySelectorAll(".modal-close").forEach((element) => element.addEventListener("click", closeModal));
+    modal.querySelectorAll(".modal-stop").forEach((element) => element.addEventListener("click", stopPropagation));
+    modal.querySelector(".modal2-open").addEventListener("click", () => {
+        modal1.style.display = "none";
+        modal2.style.display = "flex";
+        modal.querySelectorAll(".modal-wrapper").forEach((element) => element.classList.add("no-animation"));
+        focusablesElements = Array.from(modal2.querySelectorAll(focusableSelector));
+        focusablesElements[0].focus();
+        console.log(focusablesElements);
+    });
+    modal.querySelector(".modal1-back").addEventListener("click", () => {
+        modal2.style.display = "none";
+        modal1.style.display = "flex";
+        modal.querySelectorAll(".modal-wrapper").forEach((element) => element.classList.add("no-animation"));
+        focusablesElements = Array.from(modal1.querySelectorAll(focusableSelector));
+        modal.querySelector(".modal2-open").focus();
+    });
+    focusablesElements[0].focus();
 };
 
 const closeModal = () => {
@@ -30,8 +49,11 @@ const closeModal = () => {
     modal.removeEventListener("click", closeModal);
     modal.querySelector(".modal-close").removeEventListener("click", closeModal);
     modal.querySelector(".modal-stop").removeEventListener("click", stopPropagation);
+    modal.querySelectorAll(".modal-wrapper").forEach((element) => element.classList.remove("no-animation"));
     const hideModal = () => {
         modal.style.display = "none";
+        modal1.style.display = "none";
+        modal2.style.display = "none";
         modal.removeEventListener("animationend", hideModal);
         modal = null;
         galleryModal.innerHTML = "";
